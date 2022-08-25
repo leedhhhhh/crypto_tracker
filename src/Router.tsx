885239b9488284ed012cfa709701ab0aae3b60e9
이동형 <leedhhhhh@gmail.com>
2022-08-25 13:30:24 +0900

    경로수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Chart from "./routes/Chart";
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
+import Price from "./routes/Price";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/:coinId" element={<Coin></Coin>}></Route>
+        <Route path="/:coinId/*" element={<Coin></Coin>}></Route>
         <Route path="/" element={<Coins></Coins>}></Route>
+        <Route path="chart" element={<Chart />} />
+        <Route path="price" element={<Price />} />
       </Routes>
     </BrowserRouter>
   );
