439f31e1e44ac5d50341b83aece3d4280b0a6b8b
이동형 <leedhhhhh@gmail.com>
2022-08-26 15:50:22 +0900

    코인차트용 api

--- src/api.tsx
+++ src/api.tsx
@@ -1,4 +1,5 @@
 const BASE_URL = `https://api.coinpaprika.com/v1`;
+const CHART_URL = `https://ohlcv-api.nomadcoders.workers.dev`;
 
 export function fetchCoins() {
   return fetch(`${BASE_URL}/coins`).then((response) => response.json());
@@ -12,6 +13,14 @@ export function fetchCoinInfo(coinId: string | undefined) {
 
 export function fetchCoinTickers(coinId: string | undefined) {
   return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
+    response.json()
+  );
+}
+
+export function fetchCoinHistory(coinId: string | undefined) {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7; // 일주일전
+  return fetch(`${CHART_URL}/?coinId=${coinId}`).then((response) =>
     response.json()
   );
 }
